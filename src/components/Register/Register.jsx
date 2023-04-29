4611a48d6aa4437e2e298940144bda893cd0cfc9
TakibHossen <mdtakibhossen1@gmail.com>
2023-04-30 03:04:10 +0600

    added fire fire

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import app from '../../firebase/firebase.config';
+
+const auth = getAuth(app);
 
 const Register = () => {
+    const [error, setError] = useState('');
     const [email, setEmail] = useState('');
 
     const handleSubmit = (event) => {
@@ -8,6 +13,17 @@ const Register = () => {
         const email = event.target.email.value;
         const password = event.target.password.value;
         // console.log(email, password)
+        createUserWithEmailAndPassword(auth, email, password)
+        .then(result => {
+            const loggedUser = result.user;
+            console.log(loggedUser)
+            setError('');
+            event.target.reset();
+        })
+        .catch(error => {
+            console.error(error)
+            setError(error.message);
+        })
     }
 
     const handleEmailChange = (event) =>{
@@ -18,15 +34,16 @@ const Register = () => {
 
     }
     return (
-        <div>
-            <h2>Register</h2>
+        <div className='w-50 mx-auto'>
+            <h2 className='text-danger'>Register</h2>
             <form onSubmit={handleSubmit}>
-                <input onChange={handleEmailChange} type="email" name='email' id='email' placeholder='Your Email' />
+                <input className='w-50 mt-3' onChange={handleEmailChange} type="email" name='email' id='email' placeholder='Your Email' />
                 <br />
-                <input onBlur={handlePasswordBlur} type="Password" name='password' id='password' placeholder='Your Password' />
+                <input className='w-50 mt-3 mb-3' onBlur={handlePasswordBlur} type="Password" name='password' id='password' placeholder='Your Password' />
                 <br />
-                <input type="submit" value="Register" />
+                <input className='btn btn-primary' type="submit" value="Register" />
             </form>
+            <p className='text-danger'>{error}</p>
         </div>
     );
 };
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD5VFBZrbLgZqlOFWE9HLIHNWH4V_ic9ag",
+  authDomain: "fir-email-password-afdf8.firebaseapp.com",
+  projectId: "fir-email-password-afdf8",
+  storageBucket: "fir-email-password-afdf8.appspot.com",
+  messagingSenderId: "456517601699",
+  appId: "1:456517601699:web:1efbfdb5cf4f322da0f216"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
